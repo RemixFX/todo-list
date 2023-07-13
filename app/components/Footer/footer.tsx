4307029f131e38a64f559e58028b576b581a6aa5
RemixFX <remix.master@yandex.ru>
2023-07-13 12:57:17 +0300

    refactor: fix a syntax typo

--- app/components/Footer/footer.tsx
+++ app/components/Footer/footer.tsx
@@ -4,7 +4,7 @@ import styles from './footer.module.css'
 export default function Footer () {
   return(
     <footer className={styles.footer}>
-    <p> 2023 Todo List Тестовое задание для компании Фабрика компонентов</p>
+    <p> 2023 Todo List Тестовое задание для компании Фабрика приложений</p>
     <div className={styles.container}>
     <p>&copy; Роман Суханов</p>
     <Link className={styles.link} href='https://github.com/RemixFX/todo-list#readme'>GitHub</Link>
